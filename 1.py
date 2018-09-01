cad76a1d978c5fb1be476350120ffa209903a751
rickywangruiqi <rickywangruiqi@didichuxing.com>
2018-09-01 13:54:32 +0800

    Add beter func

--- 1.py
+++ 1.py
@@ -19,5 +19,18 @@ class Solution(object):
             else:
                 if j in m and len(m[j])>=2:
                     return m[j][:2]
+    def better(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dic = {}
+        for i in range(0,len(nums)):
+            if target-nums[i] in dic:
+                return [dic[target-nums[i]], i]
+            dic[nums[i]] = i
+                
+    
 
 print Solution().twoSum([1,1,1,4],2)
